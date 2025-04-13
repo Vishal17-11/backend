72f54adffce788c64a43e1f7a7d8d220244c3fbb
Vishal17-11 <vishalchinnusamy11@gmail.com>
2025-04-13 21:26:16 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login } = require('./controllers/authcontroller.js');
+const { register, login } = require('../controllers/authcontroller.js');
 
 const router = express.Router();
 
