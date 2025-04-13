26979250b2a39d27f9c9f61e9d97a625e228cfb1
Vishal17-11 <vishalchinnusamy11@gmail.com>
2025-04-13 21:27:35 +0530

    Update fileRoutes.js

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fileController = require('./controllers/filecontroller');
+const fileController = require('../controllers/filecontroller');
 const authMiddleware = require('./middlewares/authMiddleware');
 
 const router = express.Router();
